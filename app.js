8bf7e2c3ac7a5df2a891b5ca3d563fc8b6b92ace
ajay rajput <ajaymmcc@gmail.com>
2021-08-16 22:40:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var btntranslate = document.querySelector("#translate");
+var textinputer  = document.querySelector("#textinput");
+var textoutputer = document.querySelector("#output");
+
+var serverURl = "https://api.funtranslations.com/translate/minion.json"
+
+function gettranslatedurl(input){
+     return serverURl + "?" + "text=" +input
+
+}
+
+function errorHandler(error){
+  console.log("error occured" ,error)
+   alert("wrong")
+}
+
+function clickhandler() {
+   var inputtext = textinputer.value;
+
+
+fetch(gettranslatedurl(inputtext))
+  .then(response=> response.json())
+  .then(json => {
+      var translatedtext = json.contents.translated;
+      textoutputer.innerText= translatedtext;})
+  .catch(errorHandler) 
+};
+btntranslate.addEventListener("click" , clickhandler)
